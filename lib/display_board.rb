c41704ee765132a0e96cd9c056bf76d1387beaa3
Daniel Correa <dcorrea2207@gmail.com>
2020-02-01 18:51:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,15 @@
 def display_board(the_board)
-  puts "#{the_board[0]} | #{the_board[1]} | ${the_board[2]}"
+  puts "#{the_board[0]} | #{the_board[1]} | #{the_board[2]}"
   puts "-----------"
-  puts "#{the_board[0]} | #{the_board[1]} | ${the_board[2]}"
-  
-  puts "#{the_board[0]} | #{the_board[1]} | ${the_board[2]}"
+  puts "#{the_board[3]} | #{the_board[4]} | #{the_board[5]}"
+  puts "-----------"
+  puts "#{the_board[6]} | #{the_board[7]} | #{the_board[8]}"
+end
+
+display_board(turn_1)
+
+turn_2 = ["x", "", "", "", "O", "", "", "", ""]
+
+display_board(turn_2)
+
+move("x", 1)
