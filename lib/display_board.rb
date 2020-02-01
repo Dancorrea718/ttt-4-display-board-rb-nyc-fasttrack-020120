c9e1beaa78cdf22fb2b8f657ceef0db0e4126d68
Daniel Correa <dcorrea2207@gmail.com>
2020-02-01 18:47:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,6 @@
 def display_board(the_board)
   puts "#{the_board[0]} | #{the_board[1]} | ${the_board[2]}"
+  puts "-----------"
+  puts "#{the_board[0]} | #{the_board[1]} | ${the_board[2]}"
+  
   puts "#{the_board[0]} | #{the_board[1]} | ${the_board[2]}"
